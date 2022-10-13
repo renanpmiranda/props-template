4cef5ec5f9b29413a0820077f14264e6eabb502d
renanpmiranda <renanpmiranda5@gmail.com>
2022-10-13 11:48:23 -0300

    a032_exercicios

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import Garagem from "./Componentes/Garagem";
 import "./styles.css";
 
 export default function App() {
+    function apresentaGaragem1(){
+    alert(`Boas vindas à garagem de Ammal`)
+  }
+  function apresentaGaragem2(){
+    alert(`Boas vindas à garagem de Renan`)
+  }
   return (
     <div className="App">
-      <Garagem />
+      <Garagem nome={"Ammal"} mensagemApresentacao={apresentaGaragem1}/>
+
+      <Garagem nome={"Renan"} mensagemApresentacao={apresentaGaragem2}/>
     </div>
   );
 }
